773c1509a3aebb82f0e6be7957fdc9815163dda2
MairaAshfaq05 <maira29.ashfaq@gmail.com>
2025-06-11 17:53:48 +0500

    commit 2

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ public:
         brushmgr = b;
         pencilmgr = p;
     }
+
     virtual ~writingtools() {}
 };
 
@@ -98,3 +99,84 @@ public:
         return colors[selectedcolour];
     }
 };
+
+class canvas {
+private:
+    RenderTexture2D target;
+    // Undo/redo stacks
+    static const int maxstates = 100;
+    Image states[maxstates];
+    int stateindex;
+    int statecount;
+    int width, height;
+public:
+    canvas(int w, int h) {
+        width = w;
+        height = h;mm
+        target = LoadRenderTexture(width, height); //raylib funtion
+        BeginTextureMode(target); //raylib function to begin drawing
+        ClearBackground(RAYWHITE);
+        EndTextureMode();
+        stateindex = 0;
+        statecount = 0;
+    }
+
+    void clear() {
+        BeginTextureMode(target);
+        ClearBackground(RAYWHITE);
+        EndTextureMode();
+    }
+
+    void push_state() {
+        Image img = LoadImageFromTexture(target.texture);
+        if (statecount < maxstates) {
+            states[statecount++] = ImageCopy(img);
+            stateindex = statecount - 1;
+        }
+        else {
+            for (int i = 1; i < maxstates; i++) states[i - 1] = states[i];
+            states[maxstates - 1] = ImageCopy(img);
+            stateindex = maxstates - 1;
+        }
+        UnloadImage(img);
+    }
+
+};
+
+class button_management {
+public:
+    Rectangle save_btn, load_btn, clear_btn, undo_btn, redo_btn;
+    bool hover_save, hover_load, hover_clear, hover_undo, hover_redo;
+    bool show_saved_msg, show_loaded_msg;
+
+
+    button_management() {
+        save_btn = { 950, 560, 45, 22 };
+        load_btn = { 1000, 560, 45, 22 };
+        clear_btn = { 950, 590, 45, 22 };
+        undo_btn = { 1000, 590, 45, 22 };
+        redo_btn = { 1050, 590, 45, 22 };
+        hover_save = hover_load = hover_clear = hover_undo = hover_redo = false;
+    }
+};
+
+
+
+
+
+
+int main() {
+    int width = 1100, height = 700;
+    InitWindow(width, height, "bscs24152 oop project");
+    SetTargetFPS(120);
+    brush_manager brush_mgr;
+    pencil_manager pencil_mgr;
+    brush_mgr.setmanagers(&brush_mgr, &pencil_mgr);
+    pencil_mgr.setmanagers(&brush_mgr, &pencil_mgr);
+    writingtools* tool_ptrs[2] = { &brush_mgr, &pencil_mgr };
+
+    while (!WindowShouldClose()) { //(KEY_ESCAPE pressed or windows close icon clicked)
+    }
+    CloseWindow();
+}
+
